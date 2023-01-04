c70eeac44460c86c4fb58f0374877ae392ed06b3
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-01-04 13:56:35 -0500

    add comments on shared state

--- utils/sharedState.ts
+++ utils/sharedState.ts
@@ -12,8 +12,18 @@ export interface StateInterface {
     width: number;
     height: number;
     rotation: number;
-  };
+  }
 }
+
+//TODO: Shared state which is combined from partner and local state
+// send shared state to partner on change
+// receive shared state from partner on change
+
+// fake shared state by using operations like sum, union, intersection, etc on local and partner state
+//IE:
+// local state: {a: 1, b: 2, c: 3}
+// partner state: {a: 1, b: 2, d: 4}
+// shared state: {a: 1, b: 2, c: 3, d: 4}
 
 export const defaultState: StateInterface = {
   message: "Hello World",
@@ -29,5 +39,5 @@ export const defaultState: StateInterface = {
     width: 30,
     height: 30,
     rotation: 0,
-  },
+  }
 };
